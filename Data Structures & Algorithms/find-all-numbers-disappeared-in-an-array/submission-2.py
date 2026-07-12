e9b9b31160d5b27976bf5b15022453b8cfe598f9
Joel Joseph R <joeljosephrival@gmail.com>
2026-07-12 19:01:57 +0530

    Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-0.py

--- Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-2.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        trace = set(nums)
-        return [i for i in range(1, len(nums) + 1) if i not in trace]
